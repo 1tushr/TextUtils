b15c23e59e2be1550b26b3c4e696f80c4ee75ddd
Tushar Verma <tuhsar2022@gmail.com>
2023-06-20 23:14:13 +0530

    copy to clipboard

--- src/components/textForm.jsx
+++ src/components/textForm.jsx
@@ -9,27 +9,30 @@ export default function TextForm(props) {
     setText(e.target.value);
   };
 
-  const handleUpclick =(event)=>{
-    let utext=text.toUpperCase();
-  setText(utext);
-  }
+  const handleUpclick = (event) => {
+    let utext = text.toUpperCase();
+    setText(utext);
+  };
 
-   const handleLoclick=(event)=>{
-    let ltext=text.toLowerCase();
+  const handleLoclick = (event) => {
+    let ltext = text.toLowerCase();
     setText(ltext);
-  }
-  const countWords=()=>{
-    if (text.trim()===""){
+  };
+  const countWords = () => {
+    if (text.trim() === "") {
       return 0;
     }
-    return text.trim().split(/\s+/).length
+    return text.trim().split(/\s+/).length;
+  };
+
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(text);
+    alert("Text Copied to Clipboard")
   }
 
-const resetText=()=>{
-  setText("Enter your text here");
-
-}
-
+  const resetText = () => {
+    setText("Enter your text here");
+  };
 
   return (
     <div>
@@ -38,10 +41,8 @@ const resetText=()=>{
         <label
           htmlFor="exampleFormControlTextarea1"
           className="form-label"
-
-        
         ></label>
-       
+
         <div>
           <textarea
             className="form-control"
@@ -53,20 +54,29 @@ const resetText=()=>{
           />
         </div>
       </div>
-      <button className="btn btn-primary mx-3" onClick={handleUpclick}>Convert to UpperCase</button>
-      <button className="btn btn-primary" onClick={handleLoclick}>Convert to LowerCase</button>
-      <button className="btn btn-success mx-3 "onClick={resetText}>Reset Text</button>
+      <button className="btn btn-primary mx-3" onClick={handleUpclick}>
+        Convert to UpperCase
+      </button>
+      <button className="btn btn-primary" onClick={handleLoclick}>
+        Convert to LowerCase
+      </button>
+      <button className="btn btn-primary mx-3" onClick={handleCopy}>
+        Copy Text
+      </button>
+      <button className="btn btn-primary mx-3 " onClick={resetText}>
+        Reset Text
+      </button>
       <div className="my-3 mx-3">
-      <h2 >Your Text Summary </h2>
-      <p>{countWords()} <b>words and</b>  {text.length} <b>characters</b></p>
-      <p>{0.008*countWords()} <b>Minutes Read</b></p>
-      <h2 className="my-3">Preview</h2>
+        <h2>Your Text Summary </h2>
+        <p>
+          {countWords()} <b>words and</b> {text.length} <b>characters</b>
+        </p>
+        <p>
+          {0.008 * countWords()} <b>Minutes Read</b>
+        </p>
+        <h2 className="my-3">Preview</h2>
         {text}
       </div>
-
-
-
-
     </div>
   );
 }
